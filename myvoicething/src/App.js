b03c177654dc1290b8565f267c37a16b134cdf57
mikesinc <mas.sinclair@gmail.com>
2019-01-09 20:02:27 +0800

    Inital Commit
    Voice Recognition App based on IBM Watson Speech-To-Text API.

---
+++ myvoicething/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+    onClick() {    
+    fetch("http://localhost:3002/api/speech-to-text/token")
+    .then(response => {
+      return response.text();
+    }).then(token => {
+    var stream = recognizeMic({
+      url: 'wss://gateway-syd.watsonplatform.net/speech-to-text/api', //overwrite URL to match region
+      access_token: token, // use `access_token` as the parameter name if using an RC service
+      Object_Mode: true,
+      interim_results: true,
+      extractResults: true
+    });
+    stream.on('data', data => {
+        this.setState({
+          text: data.alternatives[0].transcript
+        })
+    });
+    stream.on('error', err => {
+        console.log(err);
+    });
+    document.querySelector('#stop').onclick = stream.stop.bind(stream);
+  }).catch(error => {
+      console.log(error);
+  });
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <button className="btn" onClick={this.onClick.bind(this)}>Speech-to-Text, Click me and say something!</button>  
+          <div className='txt'>{this.state.text}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
