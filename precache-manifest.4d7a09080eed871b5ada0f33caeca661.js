bdc55975e7e9211dd561aa94adf39d4a861f8f97
mikesinc <mas.sinclair@gmail.com>
2019-01-11 22:35:28 +0800

    Updates

---
+++ precache-manifest.4d7a09080eed871b5ada0f33caeca661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcc7e70c738981b52da2",
+    "url": "/Voice-Recognition/static/js/runtime~main.bcc7e70c.js"
+  },
+  {
+    "revision": "eca657d2220865a71aab",
+    "url": "/Voice-Recognition/static/js/main.eca657d2.chunk.js"
+  },
+  {
+    "revision": "b3622a3163825e539609",
+    "url": "/Voice-Recognition/static/js/1.b3622a31.chunk.js"
+  },
+  {
+    "revision": "eca657d2220865a71aab",
+    "url": "/Voice-Recognition/static/css/main.8cdd704d.chunk.css"
+  },
+  {
+    "revision": "6aae1d7eee7f21a44ef6544b27d67cc9",
+    "url": "/Voice-Recognition/index.html"
+  }
+];
