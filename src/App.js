4a12acce7ad7259cd6f1e39c48793a1ac6811d5d
mikesinc <mas.sinclair@gmail.com>
2019-08-04 21:24:10 +0800

    rework

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
+import Container from 'react-bootstrap/Container';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
+import './App.css';
 
 const App = () => {
   const [text, setText] = useState("");
+  const [listening, setIsListening] = useState(false);
 
   const handleClick = () => {
+    setIsListening(true);
     axios({
       method: 'get',
-      url: 'https://my-voice-app.mybluemix.net/api/speech-to-text/token'
-      // url: 'http://localhost:3002/api/speech-to-text/token'
+      url: 'https://dry-bastion-82378.herokuapp.com/api/speech-to-text/token'
     })
       .then(token => {
         const stream = recognizeMic({
@@ -22,26 +26,49 @@ const App = () => {
           extractResults: true
         });
         stream.on('data', data => {
-          console.log(data);
           setText(data.alternatives[0].transcript)
-          console.log(text);
         });
         stream.on('error', err => {
           console.log(err);
           alert(err);
         });
-        // document.querySelector('#stop').onclick = stream.stop.bind(stream);
+        document.querySelector('#stop').onclick = stream.stop.bind(stream);
       }).catch(err => {
         console.log(err);
         alert(err);
       });
   }
 
+  useEffect(() => {
+    if (!listening) {
+      setText("");
+    }
+  })
+
   return (
-    <div className="App">
-      <button className="btn" onClick={() => handleClick()}>Speech-to-Text, Click me and say something!</button>
-      <div className='txt'>{text}</div>
-    </div >
+    <Container fluid className="App">
+      <Container fluid className="icons">
+        <a href="https://github.com/mikesinc" target='_blank' rel='noopener noreferrer'>
+          <img className='shortIcon' src='https://avatars2.githubusercontent.com/u/28840236?s=460&v=4' height="40vw" weight="40vw" alt='git'></img>
+        </a>
+        <a href="https://www.ibm.com/cloud" target='_blank' rel='noopener noreferrer'>
+          <img className='shortIcon' src={`${require('./assets/images/IBM.png')}`} height="40vw" weight="40vw" alt='git'></img>
+        </a>
+      </Container>
+      <Container fluid className="credits">
+        <h1>Powered by IBM Watson&#8482;</h1>
+        <h1>Website created by Michael Sinclair</h1>
+      </Container>
+      <Container className="text">{text}</Container>
+      <ButtonGroup className="buttons" size="lg" vertical>
+        {
+          !listening
+            ? <Button variant="dark" onClick={() => handleClick()}>Start Listening</Button>
+            : <Button variant="dark" id="stop" onClick={() => setIsListening(false)}>Stop listening</Button>
+        }
+      </ButtonGroup>
+      <Container fluid className="clouds"></Container>
+    </Container>
   )
 }
 
