a31127444e5ed080ad961bb2573a0d0a40042659
mikesinc <mas.sinclair@gmail.com>
2019-08-04 17:14:11 +0800

    fixes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
 
-    onClick() {    
-    fetch("https://my-voice-app.mybluemix.net/api/speech-to-text/token")
-    .then(response => {
-      return response.text();
-    }).then(token => {      
-    var stream = recognizeMic({
-      url: 'wss://gateway-syd.watsonplatform.net/speech-to-text/api', //overwrite URL to match region
-      access_token: token, // use `access_token` as the parameter name if using an RC service
-      Object_Mode: true,
-      interim_results: true,
-      extractResults: true
-    });
-    stream.on('data', data => {
-        this.setState({
-          text: data.alternatives[0].transcript
-        })
-    });
-    stream.on('error', err => {
+const App = () => {
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    axios({
+      method: 'get',
+      url: 'https://my-voice-app.mybluemix.net/api/speech-to-text/token'
+      // url: 'http://localhost:3002/api/speech-to-text/token'
+    })
+      .then(token => {
+        const stream = recognizeMic({
+          // url: 'wss://gateway-syd.watsonplatform.net/speech-to-text/api', //overwrite URL to match region
+          url: 'https://gateway-syd.watsonplatform.net/speech-to-text/api',
+          access_token: token.data, // use `access_token` as the parameter name if using an RC service
+          Object_Mode: true,
+          interim_results: true,
+          extractResults: true
+        });
+        stream.on('data', data => {
+          console.log(data);
+          setText(data.alternatives[0].transcript)
+          console.log(text);
+        });
+        stream.on('error', err => {
+          console.log(err);
+          alert(err);
+        });
+        // document.querySelector('#stop').onclick = stream.stop.bind(stream);
+      }).catch(err => {
         console.log(err);
-    });
-    document.querySelector('#stop').onclick = stream.stop.bind(stream);
-  }).catch(error => {
-      console.log(error);
-  });
+        alert(err);
+      });
   }
 
-  render() {
-    return (
-      <div className="App">
-          <button className="btn" onClick={this.onClick.bind(this)}>Speech-to-Text, Click me and say something!</button>  
-          <div className='txt'>{this.state.text}</div>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <button className="btn" onClick={() => handleClick()}>Speech-to-Text, Click me and say something!</button>
+      <div className='txt'>{text}</div>
+    </div >
+  )
 }
 
 export default App;
